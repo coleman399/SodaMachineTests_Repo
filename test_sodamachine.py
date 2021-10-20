97a1c5b25f465a7a66ee50788ae6e380db05f9d4
Dillon Coleman <coleman399@gmail.com>
2021-10-20 10:56:09 -0700

    finished the TestFillInventory Class

--- test_sodamachine.py
+++ test_sodamachine.py
@@ -8,25 +8,25 @@ class TestFillRegister(unittest.TestCase):
         self.soda_machine = SodaMachine()
 
     def test_fill_register(self):
-        """Verify proper amount of coins are being added to register list"""
-        self.register = self.soda_machine.register
+        """verify proper amount of coins are being added to register list"""
+        register = self.soda_machine.register
 
         quarters = 0
         dimes = 0
         nickels = 0
         pennies = 0
  
-        print(f"The length of register equals {len(self.register)}")
-        for coin in range(0, len(self.register)):
+        print(f"The length of the register is {len(register)}.")
+        for coin in range(0, len(register)):
             with self.subTest("Subtest", coin = coin):
                 # supposed to be 8 quarters
-                if self.register[coin].name == 'Quarter':
+                if register[coin].name == 'Quarter':
                     quarters += 1  
                 # supposed to be 10 dimes
-                elif self.register[coin].name == 'Dime':
+                elif register[coin].name == 'Dime':
                     dimes += 1
                 # supposed to be 20 nickels
-                elif self.register[coin].name == 'Nickel':
+                elif register[coin].name == 'Nickel':
                     nickels += 1
                 #should be 50 pennies
                 else:
@@ -36,6 +36,39 @@ class TestFillRegister(unittest.TestCase):
         result = [quarters, dimes, nickels, pennies]
 
         self.assertEqual(expected_result, result)
-                        
+
+class TestFillInventory(unittest.TestCase):
+    """test fill_inventory method in SodaMachine Class"""
+
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+    
+    def test_fill_inventory(self):
+        """verify proper amount of cans are added to inventory list"""
+
+        inventory = self.soda_machine.inventory
+
+        cola = 0
+        orange_soda = 0
+        root_beer = 0
+
+        print(f"The length of the inventory is {len(inventory)}.")
+        for can in range(0, len(inventory)):
+            with self.subTest("Subtest", can = can):
+                # supposed to be 10 Cola
+                if inventory[can].name == 'Quarter':
+                    cola += 1
+                # supposed to be 10 Orange Soda
+                elif inventory[can].name == 'Dime':
+                    orange_soda += 1
+                # supposed to be 10 Root Beer
+                else:
+                    root_beer += 1
+
+        expected_result = [10, 10, 10]
+        result = [cola, orange_soda, root_beer]
+
+        self.assertEqual(expected_result, result)
+                       
 if __name__ == '__main__':
     unittest.main()
