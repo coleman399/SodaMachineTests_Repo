e12cb766fadb8a2a40480d780d5730a9312b4969
Dillon Coleman <coleman399@gmail.com>
2021-10-20 10:40:44 -0700

    finished test_fill_register method.

--- test_sodamachine.py
+++ test_sodamachine.py
@@ -1,45 +1,41 @@
-
-
-from typing import Counter
 import  unittest
 from soda_machine import SodaMachine
-
 
 class TestFillRegister(unittest.TestCase):
     """Tests for SodaMachine's fill_register function"""
 
     def setUp(self):
-        """Sets up testing environment by instantiation of a sodamachine"""
         self.soda_machine = SodaMachine()
 
     def test_fill_register(self):
-        """Verify proper amount of quarters are being added to list"""
-        expected_result = [8, 10, 20, 50]
+        """Verify proper amount of coins are being added to register list"""
+        self.register = self.soda_machine.register
+
         quarters = 0
         dimes = 0
         nickels = 0
         pennies = 0
-       
-
-        register = self.soda_machine.register
-        print ('register)
-        # for i in len(register):
-        #     # supposed to be 8 quarters
-        #     if register[i] == 'Quarter':
-        #         quarters += 1  
-        #     # supposed to be 10 dimes
-        #     elif register[i] is 'Dime':
-        #         dimes += 1
-        #     # supposed to be 20 nickels
-        #     elif register[i] is 'Nickel':
-        #         nickels += 1
-        #     #should be 50 pennies
-        #     else:
-        #         pennies += 1
-
-        result = [quarters, dimes, nickels, pennies]
+ 
+        print(f"The length of register equals {len(self.register)}")
+        for coin in range(0, len(self.register)):
+            with self.subTest("Subtest", coin = coin):
+                # supposed to be 8 quarters
+                if self.register[coin].name == 'Quarter':
+                    quarters += 1  
+                # supposed to be 10 dimes
+                elif self.register[coin].name == 'Dime':
+                    dimes += 1
+                # supposed to be 20 nickels
+                elif self.register[coin].name == 'Nickel':
+                    nickels += 1
+                #should be 50 pennies
+                else:
+                    pennies += 1
         
-        #self.assertEqual(expected_result, result)
+        expected_result = [8, 10, 20, 50]
+        result = [quarters, dimes, nickels, pennies]
+
+        self.assertEqual(expected_result, result)
                         
 if __name__ == '__main__':
     unittest.main()
